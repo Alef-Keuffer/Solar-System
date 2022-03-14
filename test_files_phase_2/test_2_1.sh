85416562f720270ee2c2495fa1b95809a40f7e69
Alef-Keuffer <alefkeuffer@gmail.com>
2022-03-14 21:42:42 +0000

    add: test scripts

---
+++ test_files_phase_2/test_2_1.sh
@@ -0,0 +1,3 @@
+../cmake-build-debug/generator box 2 3 box.3d
+../cmake-build-debug/engine test_2_1.xml
+rm box.3d
---
+++ test_files_phase_2/test_2_3.sh
@@ -0,0 +1,5 @@
+../cmake-build-debug/generator box 2 3 box.3d
+../cmake-build-debug/generator sphere 1 8 8 sphere.3d
+../cmake-build-debug/generator cone 1 2 4 3 cone.3d
+../cmake-build-debug/engine test_2_3.xml
+rm box.3d cone.3d sphere.3d
