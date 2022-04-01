bf6fac26b49f0d0af07bf9d4f3381a4563b099e4
Alef-Keuffer <alefkeuffer@gmail.com>
2022-04-01 21:39:58 +0100

    WIP

--- test_files_phase_2/solar_system.sh
+++ test_files_phase_2/solar_system.sh
@@ -1,3 +1,12 @@
-../bin/generator box 2 3 box.3d
-../bin/engine test_2_4.xml
-rm box.3d
+# https://www.enchantedlearning.com/subjects/astronomy/planets/
+../bin/generator sphere 32.6074 16 16 mercury.3d
+../bin/generator sphere 80.9102 16 16 venus.3d
+../bin/generator sphere 85.2686 16 16 earth.3d
+../bin/generator sphere 45.3683 16 16 mars.3d
+../bin/generator sphere 954.5323 16 16 jupiter.3d
+../bin/generator sphere 806.5623 16 16 saturn.3d
+../bin/generator sphere 341.7027 16 16 uranus.3d
+../bin/generator sphere 324.8709 16 16 neptune.3d
+../bin/generator sphere 9309.6245 16 16 sun.3d
+../bin/engine solar_system.xml
+rm mercury.3d venus.3d earth.3d mars.3d jupiter.3d saturn.3d uranus.3d neptune.3d sun.3d
